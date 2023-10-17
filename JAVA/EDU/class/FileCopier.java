71e619fc0559486aaa1acd6a77a87f9f2694fbc2
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2023-10-17 21:15:07 +0900

    review

---
+++ JAVA/EDU/class/FileCopier.java
@@ -0,0 +1,47 @@
+package work.practice.stream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopier {
+	public static void copy(String sourceFileName, String targetFileName) {
+		File file = new File(sourceFileName); // 원본 파일명
+		File copyFile = new File(targetFileName); // 복사본 파일명
+
+		try {
+			FileInputStream input = new FileInputStream(file);
+			FileOutputStream output = new FileOutputStream(copyFile);
+
+			byte[] buf = new byte[1024];
+
+			int readData;
+			while ((readData = input.read(buf)) > 0) {
+				output.write(buf, 0, readData);
+			}
+
+			input.close();
+			output.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		if(args.length<2) {
+			System.out.println("Usage: java FileCopier sourceFileName targetFileName");
+			return;
+		}
+		
+		String sourceFileName = args[0];
+		String targetFileName = args[1];
+		
+		copy(sourceFileName, targetFileName);
+	}
+}
---
+++ JAVA/EDU/class/FileCreator.java
@@ -0,0 +1,46 @@
+package work.practice.stream;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileCreator {
+	public static void create(String createFileName ,String createFileContent) {
+		File file = new File(createFileName); // 생성할 파일명
+
+		try {
+			if (!file.exists()) {
+				file.createNewFile();
+				System.out.println("파일 생성이 되었습니다.");
+			} else {
+				System.out.println("이미 존재하는 파일명입니다.");
+			}
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+
+			writer.write(createFileContent); // 내용 작성
+			writer.newLine();
+
+			writer.flush();
+			writer.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		if(args.length<2) {
+			System.out.println("Usage: java FileCreator FileName FileContent");
+			return;
+		}
+		
+		String createFileName = args[0];
+		String createFileContent = args[1];
+		
+		create(createFileName,createFileContent);
+		
+	}
+}
